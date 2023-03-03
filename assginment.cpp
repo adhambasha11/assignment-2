efa3ea727ae8d2e87df40db4d197d378c2c3c774
adhambasha11 <124687878+adhambasha11@users.noreply.github.com>
2023-03-03 22:46:05 +0200

    Add files via upload

--- assginment.cpp
+++ assginment.cpp
@@ -4,7 +4,7 @@
 #include <ctime>
 #include "point.h"
 using namespace std;
-void  center(float[], float[]);
+void  center(float[], float& x, float& y);
 void generateA(float A[]);
 void generateB(float B[]);
 int main() {
@@ -17,39 +17,40 @@ int main() {
 	a = new float[100];
 	b = new float[100];
 	
+	float xA=0, yA=0,xB=0,yB=0,a1[100];
+	generateA(a); 
+	generateB(b);
+	center(a,xA,yA);
+	center(b,xB,yB);
+
 	
-	center(a, C);
-	float aXc = C[0];
-	float aYc = C[1];
-	center(b, C);
-	float bXc = C[0];
-	float bYc = C[1];
 	for (int i = 0; i < 20; i++) {
 		float x= 5 + static_cast <float> (rand()) / static_cast <float> (RAND_MAX / 95);
 		float y= 5 + static_cast <float> (rand()) / static_cast <float> (RAND_MAX / 95);
 		p.setx(x);
 		p.sety(y);
 		p.display();
-		if (p.distance(aXc, aYc) > p.distance(bXc, bYc)) 
-			cout << " p is closer to cluster A with distance of " << p.distance(aXc, aYc)<<endl;
+		if (p.distance(xA, yA) < p.distance(xB, yB)) 
+			cout << " p is closer to cluster A with distance of " << p.distance(xA, yA)<<endl;
 		else 
-			cout << " p is closer to cluster B with distance of " << p.distance(bXc, bYc) << endl;
+			cout << " p is closer to cluster B with distance of " << p.distance(xB, yB) << endl;
 		
 	}
 	
 
 	return 0;
 }
-void center(float c[], float z[]) {
+void center(float c[], float&x,float& y) {
 	float sum = 0;
 	for (int i = 0; i < 100; i += 2)
 		sum = sum + c[i];
-	z[0] = sum / 50;
+	 x = sum / 50;
+	
 	sum = 0;
 	for (int j = 1; j < 100; j += 2)
 		sum = sum + c[j];
-	z[1] = sum / 50;
-
+	y = sum / 50;
+	
 }
 
 void generateA(float A[])
@@ -61,7 +62,7 @@ void generateA(float A[])
 }
 void generateB(float B[]) {
 	
-	for (int i = 0; i < 100; i = +2)
+	for (int i = 0; i < 100; i += 2)
 		B[i] = 20 + static_cast <float> (rand()) / static_cast <float> (RAND_MAX / 20);
 	for (int j = 1; j < 100; j += 2)
 		B[j] = 20 + static_cast <float> (rand()) / static_cast <float> (RAND_MAX / 20);
